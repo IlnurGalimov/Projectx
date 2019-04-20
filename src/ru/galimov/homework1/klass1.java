9663f58aa65ad26f40db93e89eb1dd8de20cb1c9
galim <galimov0707@gmail.com>
2019-04-20 11:46:21 +0500

    создал программу для расчета топлива

--- src/ru/galimov/homework1/klass1.java
+++
@@ -1,8 +0,0 @@
-package ru.galimov.homework1;
-
-public class klass1{
-    public static void main(String[] args) {
-
-    }
-    }
-
---
+++ src/ru/galimov/homework1/task1.java
@@ -0,0 +1,15 @@
+package ru.galimov.homework1;
+
+public class task1 {
+    public static void main(String[] args) {
+        int a=50;
+        int b=43;
+        int c;
+        if(a>b);
+        c=a*b;
+        System.out.println(c);
+
+
+    }
+    }
+
